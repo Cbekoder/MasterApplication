94f5778869f2b1d919200f8019e2268f073be402
cbekoder <cbekoder@gmail.com>
2024-04-19 19:58:28 +0500

    Added Student model

--- studentApp/models.py
+++ studentApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from mainAPP.models import *
 
-# Create your models here.
+class Student(CoreModel):
+    JINS = (
+        ('erkak', 'erkak'),
+        ('ayol', 'ayol')
+    )
+
+    TALIM_TURI = (
+        ('kunduzgi', 'kunduzgi'),
+        ('sirtqi', 'sirtqi'),
+    )
+
+    ism = models.CharField(max_length=255)
+    familiya = models.CharField(max_length=255)
+    otasi = models.CharField(max_length=255, blank=True, null=True)
+    jins = models.CharField(max_length=10, choices=JINS)
+    telefon_shaxsiy = models.CharField(max_length=13)
+    telefon_qoshimcha = models.CharField(max_length=13, blank=True, null=True)
+    guruh = models.CharField(max_length=10)
+    tuman = models.ForeignKey(District, on_delete=models.SET_NULL, null=True)
+    manzil = models.CharField(max_length=255)
+    talim_turi = models.CharField(max_length=255, choices=TALIM_TURI)
+    class Meta:
+        verbose_name = 'Talaba'
+        verbose_name_plural = 'Talabalar'
+
+    def __str__(self):
+        return f"{self.ism} {self.familiya} {self.otasi}"
